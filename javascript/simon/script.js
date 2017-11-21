58d337962184b12dc1e38baae06bdb99d4a52bc6
julien-lav <julien.laville@hotmail.fr>
2017-11-21 19:55:01 +0100

    test on simon 3

--- javascript/simon/script.js
+++ javascript/simon/script.js
@@ -4,11 +4,11 @@ var $one = document.getElementById('circle-red');
 var $two =  document.getElementById('circle-green');
 var $three = document.getElementById('circle-yellow');
 var $four = document.getElementById('circle-blue');
-
+// verifier les $
 var tab_board = [$one, $two, $three, $four];
 var current_board = [];
 var played_board = [];
-
+var count_turn = 0;
 
 $one.addEventListener('click', function() {
 	played_board.push(this);
@@ -24,7 +24,7 @@ $two.addEventListener('click', function() {
 
 $three.addEventListener('click', function() {
 	played_board.push(this);
-	console.log(played_board);
+	//console.log(played_board);
 	start_game();
 });
 
@@ -33,7 +33,8 @@ $four.addEventListener('click', function() {
 	//console.log(played_board);
 	start_game();
 });
-
+// Il faut que je factorise le haut pour comparer mon tableau current 
+//  
 
 function start_game (){
 	var random = Math.floor(Math.random()*4);
@@ -42,15 +43,20 @@ function start_game (){
 
 function ligth(random){
 		
+		current_board.push(tab_board[random]);
+		console.log(current_board);
+
 		for(var i = 0; i < current_board.length-1; i++)
 		{
 			current_board[i].classList.add('circle-ligth'); // current_board[i].classList.remove('circle-ligth');
+			//count_turn = count_turn +1;
+			//console.log(count_turn);
 		}
 			tab_board[random].classList.add('circle-ligth'); // tab_board[random].classList.remove('circle-ligth');
-		 console.log(tab_board[random]);
+		 //console.log(tab_board[random]);
 
-			current_board.push(tab_board[random]);
-		 console.log(current_board);
+			
+		
 		
 }
 
