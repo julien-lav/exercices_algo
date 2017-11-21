614f471000427699974d9f78877ae92224f9b7ed
julien-lav <julien.laville@hotmail.fr>
2017-11-21 18:52:41 +0100

    test on simon 3

--- javascript/simon/script.js
+++ javascript/simon/script.js
@@ -3,33 +3,36 @@
 var $one = document.getElementById('circle-red');
 var $two =  document.getElementById('circle-green');
 var $three = document.getElementById('circle-yellow');
-var $for = document.getElementById('circle-blue');
+var $four = document.getElementById('circle-blue');
 
-var tab_board = [$one, $two, $three, $for];
+var tab_board = [$one, $two, $three, $four];
 var current_board = [];
 var played_board = [];
 
 
-$one.onclick = function() { 
+$one.addEventListener('click', function() {
+	played_board.push(this);
+	//console.log(played_board);
+	start_game();
+});
+
+$two.addEventListener('click', function() {
+	played_board.push(this);
+	//console.log(played_board);
+	start_game();
+});
+
+$three.addEventListener('click', function() {
 	played_board.push(this);
 	console.log(played_board);
+	start_game();
+});
 
- };
-$two.onclick = function() { 
+$four.addEventListener('click', function() {
 	played_board.push(this);
-	console.log(played_board);
-
-
- };
-$three.onclick = function() { 
-	played_board.push(this);
-	console.log(played_board);
-
- };
-$for.onclick = function() { 
-	played_board.push(this);
-	console.log(played_board);
- };
+	//console.log(played_board);
+	start_game();
+});
 
 
 function start_game (){
@@ -38,28 +41,29 @@ function start_game (){
 }
 
 function ligth(random){
-
-	for(var i = 0; i < current_board.length-1; i++)
-	{
-		current_board[i].classList.add('circle-ligth'); current_board[i].classList.remove('circle-ligth');
-	}
-		tab_board[random].classList.add('circle-ligth'); tab_board[random].classList.remove('circle-ligth');
-	 console.log(tab_board[random]);
-
-		current_board.push(tab_board[random]);
-	 console.log(current_board);
-}
-
-function player_turn(){
-	tab_board.onclick = function() {
-		alert('OUI !!!!');
-		// played_bord.push
-		while(current_board === played_board){
-		randomize();
+		
+		for(var i = 0; i < current_board.length-1; i++)
+		{
+			current_board[i].classList.add('circle-ligth'); // current_board[i].classList.remove('circle-ligth');
 		}
-	}
-	alert("game over !");
+			tab_board[random].classList.add('circle-ligth'); // tab_board[random].classList.remove('circle-ligth');
+		 console.log(tab_board[random]);
+
+			current_board.push(tab_board[random]);
+		 console.log(current_board);
+		
 }
+
+//function player_turn(){
+//	tab_board.onclick = function() {
+//		alert('OUI !!!!');
+//		// played_bord.push
+//		while(current_board === played_board){
+//		randomize();
+//		}
+//	}
+//	alert("game over !");
+//}
 
 
 start_game();
