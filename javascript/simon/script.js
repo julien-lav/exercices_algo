18cd808fcc016d7163bd12ad3713cf945784d47e
julien-lav <julien.laville@hotmail.fr>
2017-11-21 16:52:12 +0100

    test on simon

---
+++ javascript/simon/script.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+var $one = document.getElementById('circle-red');
+var $two =  document.getElementById('circle-green');
+var $three = document.getElementById('circle-yellow');
+var $for = document.getElementById('circle-blue');
+
+var tab_board = [$one, $two, $three, $for];
+var current_board = [];
+var played_bord = [];
---
+++ javascript/simon/script_piste.js
@@ -0,0 +1,41 @@
+
+
+
+var $one = document.getElementById('circle-red');
+var $two =  document.getElementById('circle-green');
+var $three = document.getElementById('circle-yellow');
+var $for = document.getElementById('circle-blue');
+
+var tab_board = [$one, $two, $three, $for];
+var current_board = [];
+var played_bord = [];
+
+// var random = Math.floor(Math.random()*4);
+
+
+function randomize (){
+	var random = Math.floor(Math.random()*4);
+	return ligth(random);
+}
+function ligth(test){
+
+	tab_board[test].classList.add('circle-ligth');
+	// console.log(tab_board[test]);
+
+	current_board.push(tab_board[test]);
+
+	// console.log(current_board);
+}
+function player_turn(){
+	tab_board.onclick = function() {
+		alert('OUI !!!!');
+		// played_bord.push
+		while(current_board === played_bord){
+		randomize();
+		}
+	}
+	alert("game over !");
+}
+
+
+player_turn();
